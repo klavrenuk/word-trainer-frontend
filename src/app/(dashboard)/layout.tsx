e2ec4c2bbce607fe06c0b46aabdccf31297b4fb0
Kirill Lavrenuk <klavrenuk@yandex.ru>
2026-07-04 21:00:55 +0300

    fix auth

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,48 +1,13 @@
-'use client'
+import { redirect } from 'next/navigation'
 
-import { useRouter } from 'next/navigation'
-import { ConfigProvider } from 'antd'
-import classNames from 'classnames'
-import { Roboto } from 'next/font/google'
+import { WidgetDashboard } from '@/widgets/dashboard'
 
-import Aside from '@/shared/components/aside'
-
-import { isAuthenticated } from '@/shared/lib/auth'
-
-const roboto = Roboto({
-    weight: ['400', '500', '700'],
-    subsets: ['latin', 'cyrillic'],
-    display: 'swap'
-})
-
-export default async function DashboardLayout({
+const DashboardLayout = ({
     children
 }: Readonly<{
     children: React.ReactNode
-}>) {
-    const router = useRouter()
-
-    if (!isAuthenticated()) {
-        return router.push('/login')
-    }
-
-    const bodyClasses = classNames('min-h-full flex flex-col', roboto.className)
-    const antDesignTheme = {
-        theme: {
-            token: {
-                fontFamily: roboto.style.fontFamily
-            }
-        }
-    }
-
-    return (
-        <ConfigProvider {...antDesignTheme}>
-            <div className={bodyClasses}>
-                <div className='grid-body'>
-                    <Aside />
-                    <main>{children}</main>
-                </div>
-            </div>
-        </ConfigProvider>
-    )
+}>) => {
+    return <WidgetDashboard>{children}</WidgetDashboard>
 }
+
+export default DashboardLayout
---
+++ src/widgets/dashboard/index.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import { ConfigProvider } from 'antd'
+import classNames from 'classnames'
+import { Roboto } from 'next/font/google'
+
+import { isAuthenticated } from '@/shared/lib/auth'
+
+import Aside from '@/shared/components/aside'
+
+const roboto = Roboto({
+    weight: ['400', '500', '700'],
+    subsets: ['latin', 'cyrillic'],
+    display: 'swap'
+})
+
+interface DashboardLayoutProps {
+    children: React.ReactNode
+}
+
+export const WidgetDashboard = ({ children }: DashboardLayoutProps) => {
+    if (!isAuthenticated()) {
+        return null
+    }
+
+    const bodyClasses = classNames('min-h-full flex flex-col', roboto.className)
+    const antDesignTheme = {
+        theme: {
+            token: {
+                fontFamily: roboto.style.fontFamily
+            }
+        }
+    }
+
+    return (
+        <ConfigProvider {...antDesignTheme}>
+            <div className={bodyClasses}>
+                <div className='grid-body'>
+                    <Aside />
+                    <main className="py-5">{children}</main>
+                </div>
+            </div>
+        </ConfigProvider>
+    )
+}
+
+export default WidgetDashboard
